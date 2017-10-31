757484446d49b2a5e1ed2bab2a1189daa6c0aa5a
abelko <esung1129@gmail.com>
2017-10-31 18:20:39 +0900

    post처리

--- app1/lotto/views.py
+++ app1/lotto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -11,5 +11,14 @@ def index(request):
     return render(request, "lotto\default.html", {"lottos": lottos})
 
 def post(request):
-    form = PostForm()
-    return render(request, 'lotto/form.html', {"form": form})
+    if request.method == "POST":
+        #save data
+        form = PostForm(request.POST)
+        if form.is_valid():
+            lotto = form.save(commit = False)
+            lotto.generate()
+            return redirect('index')
+
+    else:
+        form = PostForm()
+        return render(request, 'lotto/form.html', {"form": form})
